1f9f31ce9ab57ea90cb5147a36194bb50b681742
Ollihj <ollihj@gmail.com>
2024-09-20 11:24:49 +0200

    Opgaver opgaver opgaver

--- src/opgave02/Craps.java
+++ src/opgave02/Craps.java
@@ -10,47 +10,49 @@ public class Craps {
         playCrabs(rollDice()); //metode der starter spillet
     }
 
-    public static int[] rollDice(){
+    public static int[] rollDice() {
         int[] rolls = new int[2]; //array der gemmer terninge kast
         rolls[0] = (int) (Math.random() * 6 + 1); //terninger
         rolls[1] = (int) (Math.random() * 6 + 1);
         return rolls;
     }
 
-    public static void playCrabs(int[] rolls){
+    public static void playCrabs(int[] rolls) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Tryk enter for at starte, nej for at stoppe ");
         String answer = scanner.nextLine(); //scanner der registrer user input for at slå terning
 
 
         int rollsSum = rolls[0] + rolls[1];
-        if (rollsSum == 7 || rollsSum == 11){ //Win condition på første slag
+        if (rollsSum == 7 || rollsSum == 11) { //Win condition på første slag
             System.out.println("Du slog: " + rolls[0] + " og " + rolls[1] + " du vandt");
-        }else if (rollsSum == 2 || rollsSum == 3 || rollsSum == 12){ //lose condtion på første slag
+        } else if (rollsSum == 2 || rollsSum == 3 || rollsSum == 12) { //lose condtion på første slag
             System.out.println("Du slog: " + rolls[0] + " og " + rolls[1] + " du tabte");
-        }else{
+        } else {
             point = rollsSum;
-        System.out.println("du slog " + rolls[0] + " og " + rolls[1] + " du skal nu ramme " + rollsSum + " point");
-            if (!continuegame(point));} //metode for at fortsætte spillet hvis hverken win eller lose condition opfyldes retunere false hvis spillet skal stoppes
+            System.out.println("du slog " + rolls[0] + " og " + rolls[1] + " du skal nu ramme " + rollsSum + " point");
+            if (!continuegame(point)) ;
+        } //metode for at fortsætte spillet hvis hverken win eller lose condition opfyldes retunere false hvis spillet skal stoppes
 
 
     }
-    public static boolean continuegame(int point){ //metode for at fortsætte spillet
+
+    public static boolean continuegame(int point) { //metode for at fortsætte spillet
         Scanner scan = new Scanner(System.in); //scanner der tager user input kører spillet videre
         System.out.println("Tryk enter for at rulle videre ");
         String answer = scan.nextLine();
 
-        while (answer ==""){ //loop der slår terning hver gang der registrers user input
+        while (answer == "") { //loop der slår terning hver gang der registrers user input
             int[] faces = rollDice(); //array som gemmer terningkast
             System.out.println("terning 1: " + faces[0]); //printer terning slag ud
             System.out.println("terning 2: " + faces[1]);
 
             int sum = faces[0] + faces[1]; //integer der gemmer summen af begge slag
 
-            if (sum == point){ //win condtioin
+            if (sum == point) { //win condtioin
                 System.out.println("du har vundet ");
                 return false;
-            }else if (sum == 7){//lose condition
+            } else if (sum == 7) {//lose condition
                 System.out.println("du har tabt ");
                 return false;
             }
--- src/opgave03/Pigs.java
+++ src/opgave03/Pigs.java
@@ -4,47 +4,128 @@ import java.util.Scanner;
 
 public class Pigs {
     private static int sumPlayer1 = 0;
-    private static int SumPlayer2 = 0;
+    private static int sumPlayer2 = 0;
     private static boolean gameOver = false;
     private static boolean isPlayerOne = true;
-
+    private static int roundPoints = 0;
+    private static int winningPoints = 0;
+    private static int totalRollsPlayer1 = 0;
+    private static int totalRollsPlayer2 = 0;
+    private static int turnsPlayer1 = 0;
+    private static int turnsPlayer2 = 0;
+    private static String currentplayer;
 
     public static void main(String[] args) {
+        playPigs();
 
+        System.out.println();
+
+        printStatistics();
     }
+
     private static int rollDie() {
-        int rolls = 0;
-        rolls = (int) (Math.random() * 6 + 1);
-        return rolls;
+        return (int) (Math.random() * 6 + 1);
     }
-    private static void playPigs(){
+
+    private static int[] rollTwoDice() {
+        return new int[]{rollDie(), rollDie()};
+    }
+
+    private static void playPigs() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("press enter to roll, no to stop ");
-        String answer = scanner.nextLine();
 
-        while (gameOver !=true){
+        System.out.println("Indtast antal point for at vinde: ");
+        Scanner input = new Scanner(System.in);
+        winningPoints = input.nextInt();
 
-            if (!answer.equals("no")){
-                int face = rollDie();
-                int tempsum = face;
+        while (!gameOver) {
+            System.out.println("Tryk enter for at rulle terningerne, skriv 'nej' for at stoppe ");
+            String answer = scanner.nextLine();
 
-                if (face == 1){
-                    System.out.println("you rolled 1 your turn is over ");
-                    System.out.println();
-                    tempsum =0;
+            if (!answer.equals("nej")) {
+                int[] faces = rollTwoDice();
+                int face1 = faces[0];
+                int face2 = faces[1];
+
+                if (isPlayerOne) {
+                    totalRollsPlayer1++;
+                } else {
+                    totalRollsPlayer2++;
                 }
+
+                System.out.println((isPlayerOne ? "Spiller 1" : "Spiller 2") + " - kastede " + face1 + " og " + face2);
+
+                if (face1 == 1 && face2 == 1) {
+                    System.out.println((isPlayerOne ? "Spiller 1" : "Spiller 2") + " - kastede (1,1), du mister alle dine point!");
+                    if (isPlayerOne) {
+                        sumPlayer1 = 0;
+                        isPlayerOne = false;
+                        turnsPlayer1++;
+                    } else {
+                        sumPlayer2 = 0;
+                        isPlayerOne = true;
+                        turnsPlayer2++;
+                    }
+                    roundPoints = 0;
+                } else if (face1 == 1 || face2 == 1) {
+                    System.out.println("En af terningerne viste 1, din tur er slut, og du mister pointene fra denne runde.");
+                    roundPoints = 0;
+                    if (isPlayerOne) {
+                        isPlayerOne = false;
+                        turnsPlayer1++;
+                    } else {
+                        isPlayerOne = true;
+                        turnsPlayer2++;
+                    }
+                } else {
+                    roundPoints += face1 + face2;
+                    if (isPlayerOne) {
+                        System.out.println("Dine point denne tur er " + roundPoints);
+                        System.out.println("Point i alt: " + sumPlayer1 + " (+" + roundPoints + ")");
+                    } else {
+                        System.out.println("Dine point denne tur er " + roundPoints);
+                        System.out.println("Point i alt: " + sumPlayer2 + " (+" + roundPoints + ")");
+                    }
+                }
+
+                if (winCondition()) {
+                    gameOver = true;
+                }
+            } else {
+                if (isPlayerOne) {
+                    sumPlayer1 += roundPoints;
+                    isPlayerOne = false;
+                    turnsPlayer1++;
+                } else {
+                    sumPlayer2 += roundPoints;
+                    isPlayerOne = true;
+                    turnsPlayer2++;
+                }
+                roundPoints = 0; // Nulstil runde pointene
+                System.out.println("Tur skiftet til " + (isPlayerOne ? "spiller 1" : "spiller 2"));
             }
         }
-
-
     }
-    private static boolean winCondtion(){
-        if ((sumPlayer1 ==100)){
-            System.out.println("player 1 has won");
+
+    private static boolean winCondition() {
+        if (sumPlayer1 >= winningPoints || roundPoints >= winningPoints) {
+            System.out.println();
+            System.out.println("Spiller 1 har vundet");
             return true;
-        }else if(SumPlayer2==100){
-            System.out.println("player 2 has won");
+        } else if (sumPlayer2 >= winningPoints || roundPoints >= winningPoints) {
+            System.out.println();
+            System.out.println("Spiller 2 har vundet");
+            return true;
         }
         return false;
+    }
+
+    private static void printStatistics() {
+        double averageRollsPlayer1 = (double) totalRollsPlayer1 / turnsPlayer1;
+        double averageRollsPlayer2 = (double) totalRollsPlayer2 / turnsPlayer2;
+
+        System.out.println("Statistik:");
+        System.out.println("Spiller 1 - Gennemsnitlige kast pr. tur: " + averageRollsPlayer1);
+        System.out.println("Spiller 2 - Gennemsnitlige kast pr. tur: " + averageRollsPlayer2);
     }
 }
